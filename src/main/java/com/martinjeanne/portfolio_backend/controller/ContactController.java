3a980719fcccc469de2571a6de041339aa51f100
MartinJeanne <martinjeanne.dev@gmail.com>
2025-04-08 23:57:07 +0200

    can now send email!

--- src/main/java/com/martinjeanne/portfolio_backend/controller/ContactController.java
+++ src/main/java/com/martinjeanne/portfolio_backend/controller/ContactController.java
@@ -3,8 +3,10 @@ package com.martinjeanne.portfolio_backend.controller;
 import com.martinjeanne.portfolio_backend.model.dto.ContactMessageDto;
 import com.martinjeanne.portfolio_backend.model.entity.ContactMessage;
 import com.martinjeanne.portfolio_backend.repo.ContactMessageRepo;
+import com.martinjeanne.portfolio_backend.service.EmailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactController {
 
     private final ContactMessageRepo contactMessageRepo;
+    private final EmailService emailService;
+
+    @Value("${MAIL_USERNAME}")
+    private String mailUsername;
 
     @PostMapping
     public ResponseEntity<ContactMessageDto> receiveMessage(@RequestBody @Valid ContactMessageDto cm) {
-        System.out.println("Message from " + cm.getAuthor() + " received: " + cm.getMessage());
         ContactMessage toSave = ContactMessage.builder().author(cm.getAuthor()).message(cm.getMessage()).build();
         contactMessageRepo.save(toSave);
+        emailService.sendEmail(mailUsername, "Message received on portfolio from: " + cm.getAuthor(), cm.getMessage());
         return ResponseEntity.ok(cm);
     }
 }
