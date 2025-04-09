e7e3274278587a6215155f5677c47988c225e7ef
MartinJeanne <martinjeanne.dev@gmail.com>
2025-04-09 20:56:34 +0200

    sending email in async

--- src/main/java/com/martinjeanne/portfolio_backend/service/EmailService.java
+++ src/main/java/com/martinjeanne/portfolio_backend/service/EmailService.java
@@ -3,6 +3,7 @@ package com.martinjeanne.portfolio_backend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(to);
